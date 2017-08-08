53209af6602d1f699945ea9448d401647235ed4e
lucky19p <30668398+lucky19p@users.noreply.github.com>
2017-08-08 15:15:05 +0200

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -19,8 +19,7 @@ app.get('/Artical two',function (req, res){
 
 app.get('/Artical three',function (req, res){
     res.send('Artical three requested and will be served here');
-});
-
+});    
 
 })app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
