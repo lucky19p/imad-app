870b8acb71a6d3e1b3579ccb06f1a3a8d8e87dd7
lucky19p <30668398+lucky19p@users.noreply.github.com>
2017-08-17 05:42:50 +0200

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,44 +1,12 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var pool=require('pg').pool;
 
-var app = express();
-app.use(morgan('combined'));
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
-
-app.get('/artical-one',function (req, res){
-   res.sendFile(path.join(__dirname, 'ui', 'artical-one.html'));
-});
-
-app.get('/artical-two',function (req, res){
-    res.sendFile(path.join(__dirname, 'ui', 'artical-two.html'));
-});
-
-app.get('/artical-three',function (req, res){
-    res.sendFile(path.join(__dirname, 'ui', 'artical-three.html'));
-});
-  
-
-app.get('/ui/main.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
-  
-  app.get('/ui/style.css', function (req,res){
-      
-      res.sendfile(path.join(__dirname, 'ui', 'style.css'));
-  });
-});
-
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
-
-
-// Do not change port, otherwise your app won't run on IMAD servers
-// Use 8080 only for local development if you already have apache running on 80
-
-var port = 80;
-app.listen(port, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
-});
+var config = {
+    user:'lucky19p',
+    database:'lucky19p',
+    host:'db.imad.hasura-app.io',
+    port:'5432',
+    password: process.env.DB_PASSWORD
+};
